ec878a11efc42201249d363f9ab4ae04866f69c7
= <=>
2024-05-23 18:34:00 -0300

    fixed bugs

--- hope1.0/scripts/game_over.gd
+++ hope1.0/scripts/game_over.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 func _ready():
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/mundo.tscn")
+
 func _ready():
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
