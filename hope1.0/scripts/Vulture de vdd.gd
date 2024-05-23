02100cca1f98ada219d28f98c230b3c7c266f637
lucaxgomex <lucaxgomex@gmail.com>
2024-05-23 09:28:48 -0300

    add:vulture

---
+++ hope1.0/scripts/Vulture de vdd.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+class_name Vulture
+@onready var animation_vulture = $AnimatedSprite2D
+
+var damage = 25
+const SPEED = 120.0
+
+var direction = 1
+
+@onready var ray_cast_right = $RayCast_right
+@onready var ray_cast_left = $RayCast_left
+
+func _process(delta):
+		if ray_cast_right.is_colliding():
+			direction = -1
+		if ray_cast_left.is_colliding():
+			direction = 1
+		position.x += direction * SPEED * delta
+		if direction > 0:
+			animation_vulture.flip_h = true
+		if direction < 0:
+			animation_vulture.flip_h = false
+
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		body.takeDamage(damage)
