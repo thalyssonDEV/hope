c6ad7f80809a813bff4865f2557fa129d3ccecbb
labiras <labiras@labiras.com>
2024-05-23 17:06:23 -0300

    fazendo a camfire funcional

--- hope1.0/scenes/game_over.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_file("res://scenes/mundo.tscn")
---
+++ hope1.0/scripts/game_over.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+
+#Called when the node enters the scene tree for the first time.
+func _ready():
+	await get_tree().create_timer(1).timeout
+	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
