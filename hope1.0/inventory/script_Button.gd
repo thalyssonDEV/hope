0c21989c6dfe017cf5a0ae0807ad424c25e72e08
labiras <labiras@labiras.com>
2024-05-27 17:54:49 -0300

    add:wood

--- hope1.0/inventory/script_Button.gd
+++ hope1.0/inventory/script_Button.gd
@@ -1,5 +1,8 @@
 extends Button
 
+func _ready() -> void:
+	Global.button = self
+
 #acionamento do botão e chamada da FuncItems(função responsavel pela funcionalidade do item)
 func _on_pressed():
 	var name_id = get_parent().name_item
@@ -7,14 +10,14 @@ func _on_pressed():
 	print("name: ",name_id,"quantidade de item: ",quantidade_item)
 	var result = FuncItems.use_item(name_id)
 	if result:
-		removeItem(name_id)
+		removeItem(name_id, 1)
 		
 ## Função para decrementar item do inventário caso tenha sido usado.
-func removeItem(nameId):
+func removeItem(nameId, qtd):
 	var slot = Global.invPlay.slots;
 	for item in range(slot.size()):
 		if slot[item].item.name == nameId:
-			slot[item].amount -= 1;
+			slot[item].amount -= qtd;
 			get_parent().update(slot[item])
 			return;
 		
